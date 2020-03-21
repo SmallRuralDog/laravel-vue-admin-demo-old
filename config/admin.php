c819542108dab7a6313dd00dc45e599d355e09e2
小土狗 <296404875@qq.com>
2020-03-22 00:37:47 +0800

    修改路由

--- config/admin.php
+++ config/admin.php
@@ -32,7 +32,7 @@ return [
     ],
     'bootstrap' => app_path('Admin/bootstrap.php'),
     'route' => [
-        'prefix' => env('ADMIN_ROUTE_PREFIX', 'admin'),
+        'prefix' => env('ADMIN_ROUTE_PREFIX', ''),
         'api_prefix' => env('ADMIN_ROUTE_PREFIX', 'admin-api'),
         'namespace' => 'App\\Admin\\Controllers',
         'middleware' => ['web', 'admin'],
