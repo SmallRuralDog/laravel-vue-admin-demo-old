4450df4b9f81643a9f1ed7ed6f9c7530992c9ee2
小土狗 <296404875@qq.com>
2020-03-22 17:50:33 +0800

    添加演示

--- app/Admin/Controllers/DtkSuperCategoryController.php
+++ app/Admin/Controllers/DtkSuperCategoryController.php
@@ -30,13 +30,13 @@ class DtkSuperCategoryController extends AdminController implements AdminResourc
 
         $grid->column('cid', '一级分类ID')->width(150)->sortable();
         $grid->column('cname', '一级分类名称')->width(150);
-        $grid->column('cpic', '一级分类图标')->component(Image::make()->preview()->size(30,30))->width(150)->help("图片展现模式，点击可预览大图");
+        $grid->column('cpic', '一级分类图标')->component(Image::make()->preview()->size(30, 30))->width(150)->help("图片展现模式，点击可预览大图");
         $grid->column('subcid', '二级分类Id')->width(150)->sortable();
         $grid->column('subcname', '二级分类名称')->width(150);
         $grid->column('scpic', '二级分类图标')->component(Avatar::make())->width(150);
 
         $grid->toolbars(function (Grid\Toolbars $toolbars) {
-            $toolbars->addRight(Grid\Tools\ToolButton::make("同步分类（自定义Toolbar）")->type("text")->handler('request')->uri(route('DtkSuperCategoryCollect')));
+            $toolbars->addRight(Grid\Tools\ToolButton::make("初始化分类")->type("text")->handler('request')->uri(route('DtkSuperCategoryCollect')));
             $toolbars->addRight(Grid\Tools\ToolButton::make("本演示源代码")->type("text")->handler('link')->uri('https://github.com/SmallRuralDog/laravel-vue-admin-demo/blob/master/app/Admin/Controllers/DtkSuperCategoryController.php'));
         });
 
@@ -75,6 +75,8 @@ class DtkSuperCategoryController extends AdminController implements AdminResourc
         $url = $host . '?' . http_build_query($data);
 
         $data = Http::get($url)->json();
+
+        DtkSuperCategory::query()->delete();
 
 
         collect($data['data'])->map(function ($item) {
